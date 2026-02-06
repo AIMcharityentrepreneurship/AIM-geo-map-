08079c74019cacffb0ca500fbd2f513249863527
AIMcharityentrepreneurship <isla@charityentrepreneurship.com>
2026-02-06 10:55:47 +0000

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ globe.pointsData([
   { lat: 51.5074, lng: -0.1278, size: 0.2, color: "red" },  // London
   { lat: 40.7128, lng: -74.006, size: 0.2, color: "blue" }, // NYC
 ]);
+fetch("./data.json")
+  .then(res => res.json())
+  .then(data => {
+    globe.pointsData(data);
+  });
