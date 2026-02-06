88c2bd9889ec34b4d9b378f74dd98e20171c1edb
AIMcharityentrepreneurship <isla@charityentrepreneurship.com>
2026-02-06 10:48:35 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const globe = Globe()(document.getElementById("globeViz"))
+  .globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
+  .backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png");
+
+globe.pointsData([
+  { lat: 51.5074, lng: -0.1278, size: 0.2, color: "red" },  // London
+  { lat: 40.7128, lng: -74.006, size: 0.2, color: "blue" }, // NYC
+]);
