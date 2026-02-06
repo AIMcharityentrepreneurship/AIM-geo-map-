383b747c0419381d2088f541f5fb2884043739d3
AIMcharityentrepreneurship <isla@charityentrepreneurship.com>
2026-02-06 11:39:21 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const globe = Globe().containerElement(document.getElementById("globeViz"))
+const globe = Globe()(document.getElementById("globeViz"))
   .globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
   .backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png")
   .autoRotate(true)
