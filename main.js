5cac6fd0cedc751bfebb33ba9bbc4d0f47e7a339
AIMcharityentrepreneurship <isla@charityentrepreneurship.com>
2026-02-06 11:03:56 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const globe = Globe()(document.getElementById("globeViz"))
   .globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
   .backgroundImageUrl("//unpkg.com/three-globe/example/img/night-sky.png");
 
-globe.pointsData([
-  { lat: 51.5074, lng: -0.1278, size: 0.2, color: "red" },  // London
-  { lat: 40.7128, lng: -74.006, size: 0.2, color: "blue" }, // NYC
-]);
-
+// Load your JSON
 fetch("./data.json")
   .then(res => res.json())
   .then(data => {
-    globe.pointsData(data);
+    // Map your fields to what globe.gl expects
+    const points = data.map(d => ({
+      lat: d.lat,
+      lng: d.lon,          // IMPORTANT: globe.gl uses "lng", not "lon"
+      name: d.name,
+      country: d.country,
+      type: d.type,
+      size: 0.15,
+      color: d.type === "AIM staff" ? "orange" : "dodgerblue"
+    }));
+
+    globe.pointsData(points);
   });
